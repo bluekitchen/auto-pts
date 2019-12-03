b6e622edd59465824975c26fe8e1c51c7ce5ab36
Matthias Ringwald <matthias@ringwald.ch>
2019-12-03 15:33:15 +0100

    btstack/gap: don't set BR-EDR-not-supported flag in Advertisement Flags - testing dual-mode configuration

--- ptsprojects/btstack/gap.py
+++ ptsprojects/btstack/gap.py
@@ -104,8 +104,9 @@ def set_pixits(pts):
 
     pts -- Instance of PyPTS"""
 
+    # BTstack is tested as dual-mode, don't set AdFlags.br_edr_not_supp
     ad_str_flags = str(AdType.flags).zfill(2) + \
-                   str(AdFlags.br_edr_not_supp).zfill(2)
+                   str(0).zfill(2)
     ad_str_flags_len = str(len(ad_str_flags) / 2).zfill(2)
     ad_str_name_short = str(AdType.name_short).zfill(2) + \
                         binascii.hexlify(iut_device_name)
