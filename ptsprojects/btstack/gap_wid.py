7b599b5a6551027b680d3d604d018e792e1c72a1
Matthias Ringwald <matthias@ringwald.ch>
2019-12-10 14:37:44 +0100

    btstack/gap: disable connectable for GAP/CONN/NCON/BV-2

--- ptsprojects/btstack/gap_wid.py
+++ ptsprojects/btstack/gap_wid.py
@@ -251,7 +251,13 @@ def hdl_wid_53(desc):
 
 
 def hdl_wid_54(desc):
-    hdl_wid_51(desc)
+    stack = get_stack()
+
+    btp.gap_set_nonconn()
+    btp.gap_set_gendiscov()
+
+    btp.gap_adv_ind_on(ad=stack.gap.ad)
+
     return True
 
 
@@ -453,10 +459,7 @@ def hdl_wid_121(desc):
 
 
 def hdl_wid_122(desc):
-    btp.gap_set_nonconn()
-    btp.gap_set_gendiscov()
-
-    return True
+    return hdl_wid_54(desc)
 
 
 def hdl_wid_124(desc):
